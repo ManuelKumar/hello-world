5229d3048d977efdc3675b65fa4e82119de31515
ManuelKumar <45366506+ManuelKumar@users.noreply.github.com>
2020-12-26 21:54:02 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,11 +3,11 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-("/rest")
+
 @RestController
 public class HelloController {
 
-	@RequestMapping("/get")
+	@RequestMapping("/rest")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
