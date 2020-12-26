fce2cc2c3062716cbfc3f98f4dfe4ad94dce60ed
ManuelKumar <45366506+ManuelKumar@users.noreply.github.com>
2020-12-26 22:06:32 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,7 +3,7 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class HelloController {
